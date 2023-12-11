4c84dedd07c5d785cd1d7e99f40ed47d23c67a42
Mohanty Hitesh <mohantyhitesh4495@gmail.com>
2023-12-11 21:25:35 +0530

    Array

---
+++ src/Array/Two_Sum.java
@@ -0,0 +1,31 @@
+package Array;
+
+import java.util.Arrays;
+
+// 1. Two Sum
+// URL=https://leetcode.com/problems/two-sum/
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+Example 0:
+    Input: nums = [2,7,11,15], target = 9
+    Output: [0,1]
+    Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ */
+public class Two_Sum {
+    public static int[] twoSum(int[] nums, int target) {
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                    return new int[]{i,j};
+                }
+            }
+        }
+        return new int[]{};
+    }
+    public static void main(String[] args) {
+        int[] num={2,7,11,15};
+        System.out.println(Arrays.toString(twoSum(num,9)));
+    }
+}
