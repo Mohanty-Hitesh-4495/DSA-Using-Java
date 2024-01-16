b9e569a8072344dec235bfe4edfe3a3fae871c09
Mohanty Hitesh <mohantyhitesh4495@gmail.com>
2024-01-16 07:58:11 +0530

    sorting question

--- src/Sorting/Divide_and_Conquer.java
+++ src/Sorting/Divide_and_Conquer.java
@@ -1,4 +1,5 @@
 package Sorting;
+
 // 4. Median of Two Sorted Arrays
 // URL=https://leetcode.com/problems/median-of-two-sorted-arrays/
 /*
@@ -7,15 +8,18 @@ Input: nums1 = [1,3], nums2 = [2]
 Output: 2.00000
 Explanation: merged array = [1,2,3] and median is 2.
  */
+
 public class Divide_and_Conquer {
     public static void main(String[] args) {
         int [] arr = {1,5,9,12,16,17};
         int [] arr2 = {2,3,4,10,11,15};
         System.out.println("Median = "+findMedianSortedArrays(arr,arr2));
     }
+
     static public double findMedianSortedArrays(int[] arr, int[] arr2) {
         return merge(arr,arr2);
     }
+
     static double merge (int[] arr,int[] arr2){
         int L1=arr.length;
         int L2=arr2.length;
