9a914441437cb7cdd1a8fb9dcec7746fe78105b5
Mohanty Hitesh <mohantyhitesh4495@gmail.com>
2024-01-16 07:59:00 +0530

    sorting question

--- src/Sorting/Merge_Sorted_Array.java
+++ src/Sorting/Merge_Sorted_Array.java
@@ -3,7 +3,8 @@ import java.util.Arrays;
 
 // 88. Merge Sorted Array
 // https://leetcode.com/problems/merge-sorted-array/description/
-/* Example 1:
+
+/* Example:
         Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
         Output: [1,2,2,3,5,6]
         Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
