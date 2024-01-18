792576747c078a53728a4d1572de135d2fcef477
Mohanty Hitesh <mohantyhitesh4495@gmail.com>
2024-01-18 18:26:54 +0530

    string

---
+++ src/Strings/Reverse_String.java
@@ -0,0 +1,35 @@
+package Strings;
+
+import java.util.Arrays;
+
+// 344. Reverse String
+//https://leetcode.com/problems/reverse-string/description/
+/*  Example 1:
+        Input: s = ["h","e","l","l","o"]
+        Output: ["o","l","l","e","h"]
+    Example 2:
+        Input: s = ["H","a","n","n","a","h"]
+        Output: ["h","a","n","n","a","H"]
+ */
+public class Reverse_String {
+    public static void reverseString(char[] s) {
+        int i = 0;
+        int j = s.length-1;
+        while(i<j){
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
+        }
+    }
+    public static void main(String[] args) {
+        String str = "Hitesh Mohanty";
+        char[] arr = str.toCharArray();
+        System.out.println("Before Reverse ");
+        System.out.println(Arrays.toString(arr));
+        reverseString(arr);
+        System.out.println("After Reverse ");
+        System.out.println(Arrays.toString(arr));
+    }
+}
