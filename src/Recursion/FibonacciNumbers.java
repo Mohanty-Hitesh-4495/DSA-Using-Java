352332e3578368f9f2346217d75485edb8fffc8f
Mohanty Hitesh <mohantyhitesh4495@gmail.com>
2024-01-20 15:51:54 +0530

    recursion

--- src/Recursion/FibonacciNumbers.java
+++ src/Recursion/FibonacciNumbers.java
@@ -1,17 +1,17 @@
 package Recursion;
-// 0,1,1,2,3,5,8,13,21
+
+// Fibonacci Series : 0,1,1,2,3,5,8,13,21
+
 public class FibonacciNumbers {
     public static void main(String[] args) {
         // printing 10 Fibonacci numbers
-        for(int i=0;i<10;i++){
-            System.out.println(fibonacciNum(i));
-        }
+//        for(int i=0;i<10;i++){
+            System.out.println(fibonacciNum(10));
+//        }
     }
     public static int fibonacciNum(int num){
-        if(num==0)
-            return 0;
-        if(num==1)
-            return 1;
+        if(num<2)
+            return num;
         return fibonacciNum(num-1)+fibonacciNum(num-2);
     }
 }
