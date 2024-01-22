d8d4d2c867ec568b2f3c83772fe065b29b8d579f
Mohanty Hitesh <mohantyhitesh4495@gmail.com>
2024-01-22 18:38:06 +0530

    hackerrank question

--- src/Main.java
+++ src/Main.java
@@ -1,48 +1,68 @@
 import java.util.*;
 
+// https://www.hackerrank.com/challenges/larrys-array/problem
+// Larry's Array
+/*
+    For example, if :
+              A         rotate
+        [1,6,5,2,4,3]	[6,5,2]
+        [1,5,2,6,4,3]	[5,2,6]
+        [1,2,6,5,4,3]	[5,4,3]
+        [1,2,6,3,5,4]	[6,3,5]
+        [1,2,3,5,6,4]	[5,6,4]
+        [1,2,3,4,5,6]
+         Output=YES
+ */
 public class Main {
     public static void main(String[] args) {
-//        System.out.println("Hello world!");
         List<Integer> arr = new ArrayList<>();
         arr.add(1);
-        arr.add(6);
-        arr.add(5);
         arr.add(2);
-        arr.add(4);
         arr.add(3);
+        arr.add(5);
+        arr.add(4);
         System.out.println(arr);
         System.out.println(larrysArray(arr));
     }
-    public static String larrysArray(List<Integer> A) {
-        Map<Integer,Integer> map = new HashMap<Integer, Integer>();
-
-        for(int i=0;i<A.size();i++){
-            map.put(A.get(i),i);
+    public static int map(List<Integer> arr,int index){
+        for(Integer value: arr){
+            if(value == index+1)
+                return arr.indexOf(value);
         }
-        System.out.println(map);
+        return -1;
+    }
+    public static String larrysArray(List<Integer> A) {
+
         int index=0;
         while(index<A.size()){
+
             if(A.get(index) == index+1){
                 System.out.println(index);
                 index++;
                 continue;
             }
-            int nxtMinPos = map.get(index+1);
-            if(nxtMinPos - index >=2){
 
+            int nxtMinPos = map(A,index);
+
+            if(nxtMinPos==A.size()-1 && index==A.size()-2)
+                break;
+
+            if(nxtMinPos - index >=2){
                 int temp = A.get(nxtMinPos-2);
                 A.set(nxtMinPos-2,A.get(nxtMinPos-1));
                 A.set(nxtMinPos-1,A.get(nxtMinPos));
                 A.set(nxtMinPos,temp);
-
                 System.out.println("ARR= "+A);
-
-                map.put(A.get(nxtMinPos-2),nxtMinPos-2);
-                map.put(A.get(nxtMinPos-1),nxtMinPos-1);
-                map.put(A.get(nxtMinPos),nxtMinPos);
-
-                System.out.println("Map= "+map);
             }
+
+            else if(nxtMinPos-index <= index ){
+                int temp = A.get(nxtMinPos+1);
+                A.set(nxtMinPos+1,A.get(nxtMinPos-1));
+                A.set(nxtMinPos-1,A.get(nxtMinPos));
+                A.set(nxtMinPos,temp);
+                System.out.println("ARR= "+A);
+            }
+
             else if(nxtMinPos == 1){
                 if(nxtMinPos == A.size()-1){
                     return "NO";
@@ -52,10 +72,6 @@ public class Main {
                     A.set(nxtMinPos, A.get(nxtMinPos + 1));
                     A.set(nxtMinPos + 1, temp);
                     System.out.println("ARR= " + A);
-                    map.put(A.get(0), 0);
-                    map.put(A.get(nxtMinPos), nxtMinPos);
-                    map.put(A.get(nxtMinPos + 1), nxtMinPos + 1);
-                    System.out.println("Map= " + map);
                 }
             }
         }
