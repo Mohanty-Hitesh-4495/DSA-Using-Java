9d846fe774e4a837468957b0d228358ee70b0cf5
Mohanty Hitesh <mohantyhitesh4495@gmail.com>
2024-01-27 15:59:15 +0530

    sorting

--- src/Sorting/InsertionSort1.java
+++ src/Sorting/InsertionSort1.java
@@ -11,7 +11,7 @@ public class InsertionSort1 {
     }
 
     static void insertion(int [] arr){
-        for (int i=0;i<arr.length;i++){
+        for (int i=1;i<arr.length;i++){
             for(int j=i;j>0;j--) {
                 if(arr[j]<arr[j-1] ) {
                     swap(arr, j,j-1);
