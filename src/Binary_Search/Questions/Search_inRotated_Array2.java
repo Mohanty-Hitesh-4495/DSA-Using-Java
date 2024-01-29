5612d1d398268d98276c7765d50cdecb822eb812
Mohanty Hitesh <mohantyhitesh4495@gmail.com>
2024-01-29 22:03:47 +0530

    binary search

---
+++ src/Binary_Search/Questions/Search_inRotated_Array2.java
@@ -0,0 +1,49 @@
+package Binary_Search.Questions;
+
+// 81. Search in Rotated Sorted Array II
+// https://leetcode.com/problems/search-in-rotated-sorted-array-ii/description/
+/*
+    Example 1:
+        Input: nums = [2,5,6,0,0,1,2], target = 0
+        Output: true
+    Example 2:
+        Input: nums = [2,5,6,0,0,1,2], target = 3
+        Output: false
+ */
+public class Search_inRotated_Array2 {
+    public static void main(String[] args) {
+        int[] arr = {2,5,6,0,0,1,2};
+        boolean ans = search(arr,0);
+        System.out.println(ans);
+    }
+
+    public static boolean search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length-1;
+        while(start<=end){
+
+            int mid = start + (end-start) / 2;
+
+            if(nums[mid]==target)
+                return true;
+            if(nums[start]==nums[mid]){
+                start++;
+                continue;
+            }
+            if(nums[start]<=nums[mid]){
+                if(nums[start]<=target && target<=nums[mid]){
+                    end = mid-1;
+                } else{
+                    start = mid+1;
+                }
+            } else {
+                if(nums[mid]<=target && target<=nums[end]){
+                    start = mid+1;
+                } else{
+                    end = mid-1;
+                }
+            }
+        }
+        return false;
+    }
+}
